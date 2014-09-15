9249c35b7677e9f41af2335606179c4878c641bf
miki2020 <minow1980@gmail.com>
2014-09-15 08:49:58 +0200

    Update myallegro.php

--- myallegro/myallegro.php
+++ myallegro/myallegro.php
@@ -2,6 +2,27 @@
 if (!defined('_PS_VERSION_'))
   exit;
  
-class MyModule extends Module
+class MyAllegro extends Module
 {
+  public function __construct()
+  {
+    $this->name = 'myallegro';
+    $this->tab = 'administration';
+    $this->version = '1.0.0';
+    $this->author = 'Michał Nowak';
+    $this->need_instance = 0;
+    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_); 
+    $this->bootstrap = true;
+ 
+    parent::__construct();
+ 
+    $this->displayName = $this->l('My Allegro Integrator');
+    $this->description = $this->l('Basic Allegro WebApi functions.');
+ 
+    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+ 
+    if (!Configuration::get('MYALLEGRO_NAME'))      
+      $this->warning = $this->l('No name provided');
+  }
+  
 }
