dd15baf8a8c3df4689340c0dc827ad5cfc1f6cc3
Michał Nowak <minow1980@gmail.com>
2014-09-17 10:49:17 +0200

    Update myallegro.php

--- myallegro/myallegro.php
+++ myallegro/myallegro.php
@@ -178,6 +178,52 @@ public function getContent()
     $helper->fields_value['MYALLEGRO_NAME'] = Configuration::get('MYALLEGRO_NAME');
     $helper->fields_value['MYALLEGRO_PASS'] = Configuration::get('MYALLEGRO_PASS');
     $helper->fields_value['MYALLEGRO_APIKEY'] = Configuration::get('MYALLEGRO_APIKEY');
+    
+ // try Allewgro   
+    //define('COUNTRY_CODE', 1);
+    define('COUNTRY_CODE', 228);
+    
+define('WEBAPI_USER_LOGIN', Configuration::get('MYALLEGRO_NAME'));
+define('WEBAPI_USER_ENCODED_PASSWORD', base64_encode(hash('sha256', Configuration::get('MYALLEGRO_PASS'), true)));
+define('WEBAPI_KEY', Configuration::get('MYALLEGRO_APIKEY'));
+ 
+$options['features'] = SOAP_SINGLE_ELEMENT_ARRAYS;
+try {
+    $soapClient = new SoapClient('https://webapi.allegro.pl/service.php?wsdl', $options);
+    $request = array(
+        'countryId' => COUNTRY_CODE,
+        'webapiKey' => WEBAPI_KEY
+    );
+    $result = $soapClient->doQueryAllSysStatus($request);
+ 
+    $versionKeys = array();
+    foreach ($result->sysCountryStatus->item as $row) {
+        $versionKeys[$row->countryId] = $row;
+    }
+ 
+    $request = array(
+        'userLogin' => WEBAPI_USER_LOGIN,
+        'userHashPassword' => WEBAPI_USER_ENCODED_PASSWORD,
+        'countryCode' => COUNTRY_CODE,
+        'webapiKey' => WEBAPI_KEY,
+        'localVersion' => $versionKeys[COUNTRY_CODE]->verKey,
+    );
+    $session = $soapClient->doLoginEnc($request);
+ 
+    $request = array(
+        'sessionId' => $session->sessionHandlePart,
+        'pageSize' => 50
+    );
+ 
+    $myWonItems = $soapClient->doGetMyWonItems($request);
+    var_dump($myWonItems);
+ 
+} catch(Exception $e) {
+    echo $e;
+}
+    
+    
+    
     return $helper->generateForm($fields_form);
 } 
 }
